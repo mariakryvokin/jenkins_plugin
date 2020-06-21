d956e0ac1756aebdda5957e8ff222b9fdee8f768
Mariia_Kryvokin <kruvokin@gmail.com>
2020-06-21 13:09:23 +0300

    jenkins plugin

---
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -0,0 +1,54 @@
+package io.jenkins.plugins.sample;
+
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.util.FormValidation;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import jenkins.tasks.SimpleBuildStep;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundSetter;
+
+public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
+
+    private final String name;
+    private boolean useFrench;
+
+    @DataBoundConstructor
+    public HelloWorldBuilder(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isUseFrench() {
+        return useFrench;
+    }
+
+    @DataBoundSetter
+    public void setUseFrench(boolean useFrench) {
+        this.useFrench = useFrench;
+    }
+
+    @Override
+    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
+        run.addAction(new HelloWorldAction(name));
+        if (useFrench) {
+            listener.getLogger().println("Bonjour, " + name + "!");
+        } else {
+            listener.getLogger().println("Hello, " + name + "!");
+        }
+    }
+
+}
